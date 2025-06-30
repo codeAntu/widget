ff29a9213a9f0ec59cfb86d01dd32c33c929a2d2
codeantu <codeantu@gmail.com>
2025-06-30 16:25:41 +0530

    feat: update electron-builder configuration and add linux target
    - Refactored electron-builder.json5 for cleaner syntax and added linux target configuration.
    - Updated main.ts to improve environment variable handling for public assets.
    - Added react-grid-layout for dynamic layout management in App component.
    - Enhanced App component with grid layout and basic structure.
    - Included Tailwind CSS for styling and created index.css for global styles.
    - Added Prettier configuration for consistent code formatting.
    - Created Test component for testing purposes.
    - Added type declaration for react-grid-layout module.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import { useState } from "react";
+import GridLayout from 'react-grid-layout'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const layout = [
+    { i: 'a', x: 0, y: 0, w: 1, h: 2, static: true },
+    { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+    { i: 'c', x: 4, y: 0, w: 1, h: 2 },
+  ]
 
-  return <>Hello</>;
+  return (
+    <GridLayout className='layout' layout={layout} cols={12} rowHeight={30} width={1200}>
+      <div key='a' className='border' data-grid={{ x: 0, y: 0, w: 1, h: 2, static: true }}>
+        a
+      </div>
+
+      <div
+        className='text- border'
+        key='b'
+        data-grid={{ x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 }}
+      >
+        b
+      </div>
+      <div className='border' key='c' data-grid={{ x: 4, y: 0, w: 1, h: 2 }}>
+        c
+      </div>
+    </GridLayout>
+  )
 }
 
-export default App;
+export default App
